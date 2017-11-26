b268780cef21ea2ea09a10224a4ccb8cc5fe53fe
Dominique <niquelejon@gmail.com>
2017-11-26 03:10:30 -0600

    installed react and react-dom

--- src/app.js
+++ src/app.js
@@ -1,12 +1,4 @@
-// import './utils.js';
-import subtract, { square, add  } from './utils.js';
-import isSenior, { isAdult, canDrink } from './person.js';
+import validator from 'validator';
 
-console.log('app.js is runn away');
-console.log(square(4));
-console.log(add(100, 200));
-console.log(subtract(100, 200));
-console.log()
-console.log(isAdult(99));
-console.log(canDrink(18));
-console.log(isSenior(64));
+console.log(validator.isEmail('test@gmail.com'));
+
---
+++ src/playground/import-export-ex.js
@@ -0,0 +1,13 @@
+// import './utils.js';
+import subtract, { square, add  } from './utils.js';
+import isSenior, { isAdult, canDrink } from './person.js';
+import validator from 'validator';
+
+console.log('app.js is runn away');
+console.log(square(4));
+console.log(add(100, 200));
+console.log(subtract(100, 200));
+console.log()
+console.log(isAdult(99));
+console.log(canDrink(18));
+console.log(isSenior(64));
